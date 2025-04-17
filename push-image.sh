36d3a6c7f5ed80910d03c2bc5534a84394eb0555
Anish Hirlekar <anishsaas@gmail.com>
2025-04-17 15:12:16 +0530

    Fixed parameter

--- push-image.sh
+++ push-image.sh
@@ -4,7 +4,7 @@ TAG=$3
 PROJECT_DIR=$4
 
 # login to ecr
-aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin $REPO_URI
+aws ecr get-login-password --region $REGION | docker login --username AWS --password-stdin $REPO_URI
 
 # build image
 docker build -t $REPO_URI:$TAG $PROJECT_DIR
